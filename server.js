62e234cbe8e86265993e163d3e14e32eb937b435
mani48718-ship-it <mani48718@gmail.com>
2026-02-26 03:25:18 +0530

    final deploy

--- server.js
+++ server.js
@@ -92,7 +92,8 @@ app.get('/delete/:id', async (req, res) => {
     res.redirect('/admin');
 });
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Website running at http://localhost:3000');
+app.listen(PORT, () => {
+    console.log("Server started on port " + PORT);
 });
